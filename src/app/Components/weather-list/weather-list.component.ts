8ade411dba1c4c4dd0b2026271fa3ab38d9afa30
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-08 01:12:30 +0300

    finish weather list html and navigate to weather funnction

--- src/app/Components/weather-list/weather-list.component.ts
+++ src/app/Components/weather-list/weather-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-weather-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './weather-list.component.html',
-  styleUrl: './weather-list.component.css'
+  styleUrl: './weather-list.component.css',
 })
 export class WeatherListComponent {
-
+  weatherList: any[] = [
+    { city: 'Palastine', temperature: 22 },
+    { city: 'Cairo', temperature: 25 },
+    { city: 'New York', temperature: 5 },
+  ];
+  constructor(private router: Router) {}
+  navigateToWeather(temperature: number) {
+    this.router.navigate(['weather'], { queryParams: { temperature } });
+  }
 }
