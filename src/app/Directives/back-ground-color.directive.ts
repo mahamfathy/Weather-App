20713498f00c584235334a55c718c7e5eae62822
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-08 00:46:32 +0300

    adding the set background color directive function

--- src/app/Directives/back-ground-color.directive.ts
+++ src/app/Directives/back-ground-color.directive.ts
@@ -1,11 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBackGroundColor]',
-  standalone: true
+  standalone: true,
 })
-export class BackGroundColorDirective {
+export class BackGroundColorDirective implements OnInit {
+  @Input('appBackGroundColor') temperature: number = 0;
+  constructor(private el: ElementRef) {}
 
-  constructor() { }
-
+  ngOnInit(): void {
+    this.setBackGround();
+  }
+  private setBackGround() {
+    if (this.temperature < 10) {
+      this.el.nativeElement.style.backgroundColor = 'blue';
+    } else if (this.temperature >= 10 && this.temperature <= 25) {
+      this.el.nativeElement.style.backgroundColor = 'green';
+    } else {
+      this.el.nativeElement.style.backgroundColor = 'red';
+    }
+  }
 }
