1502c16ee82ca57d7c84b69c95e3b8d79675c420
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-08 00:52:51 +0300

    finish weather component

--- src/app/Components/weather/weather.component.ts
+++ src/app/Components/weather/weather.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { BackGroundColorDirective } from '../../Directives/back-ground-color.directive';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [BackGroundColorDirective],
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
-
+  temperature: number = 15;
 }
