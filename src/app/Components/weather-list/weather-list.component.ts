4a1c25c4c07f24c38f96c214d8ae37ed33357000
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-07 23:58:03 +0300

    adding components

---
+++ src/app/Components/weather-list/weather-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-list.component.html',
+  styleUrl: './weather-list.component.css'
+})
+export class WeatherListComponent {
+
+}
