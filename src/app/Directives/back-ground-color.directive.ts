2b9e0643214cd5a960dd5e5045d83c61c07e33d4
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-08 00:37:15 +0300

    initializing background color directive

---
+++ src/app/Directives/back-ground-color.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appBackGroundColor]',
+  standalone: true
+})
+export class BackGroundColorDirective {
+
+  constructor() { }
+
+}
