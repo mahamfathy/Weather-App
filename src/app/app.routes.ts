11ff9a8e8a56f7d90fb4b64bf274216e01741c39
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-08 22:07:28 +0300

    fix the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { WeatherComponent } from './Components/weather/weather.component';
 import { WeatherListComponent } from './Components/weather-list/weather-list.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'weather', pathMatch: 'full' },
+  { path: '', redirectTo: 'weather-list', pathMatch: 'full' },
   { path: 'weather', component: WeatherComponent, title: 'Weather' },
   {
     path: 'weather-list',
     component: WeatherListComponent,
     title: 'Weather List',
   },
+  { path: '**', redirectTo: 'weather-list', title: 'Weather List' },
 ];
