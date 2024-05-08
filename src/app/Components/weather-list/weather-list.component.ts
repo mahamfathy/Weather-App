8237ae2bf388552b973be1c64987b5e503c54c91
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-08 21:29:36 +0300

    finish filter function for weather list

--- src/app/Components/weather-list/weather-list.component.ts
+++ src/app/Components/weather-list/weather-list.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-weather-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './weather-list.component.html',
   styleUrl: './weather-list.component.css',
 })
 export class WeatherListComponent {
-
+  searchCity: string = '';
+  filterSearchList: any[];
   weatherList: any[] = [
     { city: 'Palastine', temperature: 22 },
     { city: 'Cairo', temperature: 50 },
     { city: 'New York', temperature: 5 },
   ];
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.filterSearchList = this.weatherList;
+  }
   navigateToWeather(temperature: number) {
     this.router.navigate(['weather'], { queryParams: { temperature } });
+  }
+  filterWeatherList() {
+    if (!this.searchCity) {
+      this.filterSearchList = this.weatherList;
+    } else {
+      this.filterSearchList = this.weatherList.filter((cityWeather) => {
+        return cityWeather.city
+          .toLowerCase()
+          .includes(this.searchCity.toLowerCase());
+      });
+    }
   }
 }
